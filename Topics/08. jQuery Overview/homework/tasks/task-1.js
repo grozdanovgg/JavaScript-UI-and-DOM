e5f442799f86ba8efbf43b3f9302318292d1d608
grozdanovgg <grozdanovgg@gmail.com>
2017-03-24 23:16:48 +0200

    100 points

--- Topics/08. jQuery Overview/homework/tasks/task-1.js
+++ Topics/08. jQuery Overview/homework/tasks/task-1.js
@@ -1,8 +1,6 @@
-/* globals $ */
-
-/* 
-
-Create a function that takes a selector and COUNT, then generates inside a UL with COUNT LIs:   
+/*
+Create a function that takes a selector and COUNT, then 
+generates inside a UL with COUNT LIs:   
   * The UL must have a class `items-list`
   * Each of the LIs must:
     * have a class `list-item`
@@ -20,9 +18,40 @@ Create a function that takes a selector and COUNT, then generates inside a UL wi
 */
 
 function solve() {
-  return function (selector, count) {
-   
-  };
+    return function(selector, count) {
+        if ((typeof count !== 'number') || count < 1) {
+            throw Error('invalid');
+        }
+        if (!selector || Array.isArray(selector)) {
+            throw Error;
+        }
+
+        var $ulEl = $('<ul/>').addClass('items-list');
+
+        for (var i = 0; i < count; i += 1) {
+            var currentLi = $('<li/>').addClass('list-item').html(`List item #${i}`);
+            $($ulEl).append(currentLi);
+        }
+        $(selector).append($ulEl);
+    };
 };
 
 module.exports = solve;
+
+// function solve() {
+//     return function(selector, count) {
+
+//         if (!(selector && count > 1)) {
+//             throw Error('invalid');
+//         }
+
+//         var ulElement = $('<ul/>').addClass('items-list');
+
+//         for (var counter = 0; counter < count; counter += 1) {
+//             ulElement.append($('<li/>')
+//                 .addClass('list-item').text('List item #' + counter));
+//         }
+
+//         $(selector).append(ulElement);
+//     }
+// };
