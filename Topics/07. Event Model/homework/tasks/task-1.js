95864abc725151b83d7a7c2d07b7e6c88a789ebf
grozdanovgg <grozdanovgg@gmail.com>
2017-03-24 20:50:41 +0200

    50 points on BGcoder

--- Topics/07. Event Model/homework/tasks/task-1.js
+++ Topics/07. Event Model/homework/tasks/task-1.js
@@ -1,15 +1,39 @@
-/* globals $ */
+function solve() {
+    return function(input) {
+        var itemSelected;
 
-/* 
+        if (typeof input === 'string') {
+            itemSelected = document.getElementById(input)
+        }
+        var buttons = Array.from(itemSelected.getElementsByClassName('button')),
+            contents = Array.from(itemSelected.getElementsByClassName('content'));
 
-Create a function that takes an id or DOM element and:
-  
 
-*/
+        for (but of buttons) {
+            but.innerHTML = 'hide';
+            but.addEventListener('click', changeButton)
+        }
 
-function solve(){
-  return function (selector) {
-  };
+        function changeButton(event) {
+            var element = event.target,
+                nextElement = element.nextElementSibling;
+
+            while (nextElement.className !== 'button' &&
+                nextElement.className !== 'content') {
+                nextElement = nextElement.nextElementSibling;
+            }
+            if (nextElement.className === 'content') {
+                if (element.innerHTML === 'hide') {
+                    nextElement.style.display = 'none';
+                    element.innerHTML = 'show';
+                } else {
+                    nextElement.style.display = '';
+                    element.innerHTML = 'hide';
+                }
+            }
+            nextElement = element.nextElementSibling;
+        }
+    };
 };
 
 module.exports = solve;
