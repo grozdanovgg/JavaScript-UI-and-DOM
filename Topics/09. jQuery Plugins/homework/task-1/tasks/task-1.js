1cfa8d509d13628cf0d24ff858f67adc00bb426d
grozdanovgg <grozdanovgg@gmail.com>
2017-03-25 19:23:39 +0200

    start solving task

--- Topics/09. jQuery Plugins/homework/task-1/tasks/task-1.js
+++ Topics/09. jQuery Plugins/homework/task-1/tasks/task-1.js
@@ -1,7 +1,41 @@
-function solve(){
-  return function(selector){
-    
-  };
+function solve() {
+    return function(selector) {
+        var $mainDiv = $('<div/>')
+            .addClass('dropdown-list');
+        var $divValue = $('<div/>')
+            .addClass('current')
+            .attr('data-value', "")
+            .text('Select a value')
+            .appendTo($mainDiv)
+            .click(function() {
+                $divClass.show();
+            });
+        var $divClass = $('<div/>')
+            .addClass('options-container')
+            .css('position', 'absolute')
+            .hide()
+            .on('click', '.dropdown-item', buttonClicked)
+            .appendTo($mainDiv);
+        var $selector = $(selector)
+            .appendTo($mainDiv)
+            .hide()
+            .find('option')
+            .each(function(index) {
+                $('<div/>')
+                    .addClass('dropdown-item')
+                    .attr('data-value', 'value-' + (index + 1))
+                    .attr('data-index', index)
+                    .text('Option ' + (index + 1))
+                    .appendTo($divClass);
+            });
+
+        function buttonClicked() {
+            $divValue.text($(this).text());
+            $selector.val($(this).attr('data-value'));
+            $divClass.hide();
+        }
+        $('body').append($mainDiv);
+    };
 }
 
 module.exports = solve;
