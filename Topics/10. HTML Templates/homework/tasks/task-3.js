cfa3a7354abcd983edbf72c4d72ca3e1151dbb44
grozdanovgg <grozdanovgg@gmail.com>
2017-03-27 21:50:36 +0300

    update last files

--- Topics/10. HTML Templates/homework/tasks/task-3.js
+++ Topics/10. HTML Templates/homework/tasks/task-3.js
@@ -1,9 +1,35 @@
-function solve(){
-  return function(){
-    $.fn.listview = function(data){
-      
+function solve() {
+    return function() {
+        $.fn.listview = function(data) {
+            var newID = '#' + $(this).attr('data-template');
+            console.log(newID);
+
+
+        };
     };
-  };
 }
 
-module.exports = solve;
+
+
+// module.exports = solve;
+
+// function solve() {
+//     return function() {
+//         $.fn.listview = function(data) {
+//             var dataTemplateId = "#" + $(this).attr('data-template');
+
+//             var hbtemplate = $(dataTemplateId).html();
+
+//             var template = handlebars.compile(hbtemplate);
+
+//             var current = "";
+
+//             for (var i = 0, len = data.length; i < len; i += 1) {
+//                 current = template(data[i]);
+//                 $(this).append(current);
+//             }
+
+//             return this;
+//         };
+//     };
+// }
