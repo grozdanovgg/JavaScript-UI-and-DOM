7eaa0868f6868d551637766196eb79df1d0c232a
Georgi Grozdanov <grozdanovgg@gmail.com>
2017-03-27 19:27:26 +0300

    nearly solved, just saving

---
+++ Workshops/DOM-and-DOM-manupulations/image-preview/task/tasks/README-myne.md
@@ -0,0 +1,13 @@
+#Task 1: ImagesPreviewer
+
+
+        
+  * The execution of the `createImagesPreviewer(selector, items)` function should result as follows:
+    
+    * The first item from the items collection should be displayed bigger on the left
+      * With its title on top  
+      
+  * Images in the list (on the right) have some properties:
+   
+    * When `clicked`
+      * The **bigger image and its title** (on the left) are **replaced with the clicked image on the left**
--- Workshops/DOM-and-DOM-manupulations/image-preview/task/tasks/task-1.js
+++ Workshops/DOM-and-DOM-manupulations/image-preview/task/tasks/task-1.js
@@ -1,7 +1,69 @@
 /* globals module */
 function solve() {
-    return function (selector, items) {
-      
+    return function(selector, items) {
+        var root = document.querySelector(selector),
+            imgList = document.createElement('div'),
+            searchBox = document.createElement('input'),
+            imgPreview = document.createElement('div');
+
+        imgPreview.className = 'image-preview';
+        searchBox.addEventListener('input', filterImgs);
+        imgList.appendChild(searchBox);
+        /* START setting up list images in the div*/
+        for (var obj of items) {
+            var imgToAdd = document.createElement('img'),
+                imgContainer = document.createElement('div'),
+                imgTitteToAdd = document.createElement('p');
+
+            imgToAdd.title = obj.title;
+            imgToAdd.src = obj.url;
+            imgTitteToAdd.innerHTML = imgToAdd.title;
+            imgContainer.className = 'image-container';
+            imgContainer.addEventListener('mouseover', hoverImg)
+            imgContainer.addEventListener('mouseout', hoverImg)
+
+
+            imgContainer.appendChild(imgTitteToAdd);
+            imgContainer.appendChild(imgToAdd);
+            imgList.appendChild(imgContainer);
+        }
+        /* END setting up list images in the div*/
+        // console.log(imgList.children[1]);
+
+        var firstImg = document.createElement('img'),
+            imgTitleCopy = imgList.children[1].children[1].getAttribute('title'),
+            imgSrceCopy = imgList.children[1].children[1].getAttribute('src');
+        firstImg.setAttribute('title', imgTitleCopy);
+        firstImg.setAttribute('src', imgSrceCopy);
+
+        imgPreview.appendChild(firstImg);
+        root.appendChild(imgPreview);
+        root.appendChild(imgList);
+
+        function hoverImg() {
+            if (this.style.backgroundColor === "gray") {
+                this.style.backgroundColor = "";
+            } else {
+                this.style.backgroundColor = "gray";
+            }
+        };
+
+        function filterImgs() {
+            var filter = searchBox.value;
+
+            var items = imgList.getElementsByTagName("img"),
+                regexp = new RegExp(filter, 'i', 'g');
+
+            for (item of items) {
+                if (regexp.test(item.getAttribute('title'))) {
+                    item.style.display = '';
+                    // console.log(item.style.display);
+                } else {
+                    item.style.display = 'none';
+                    // console.log(item.style.display);
+                }
+            }
+        };
     };
 }
 
