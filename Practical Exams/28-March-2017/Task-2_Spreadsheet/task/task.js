4d73a962a545884c8a210a8db74c5f33864dedd2
grozdanovgg <grozdanovgg@gmail.com>
2017-03-28 21:16:11 +0300

    Last submitted solutions - 175 points total

--- Practical Exams/28-March-2017/Task-2_Spreadsheet/task/task.js
+++ Practical Exams/28-March-2017/Task-2_Spreadsheet/task/task.js
@@ -1,11 +1,16 @@
 function solve() {
 
-	return function(selector, rows, columns) {
-	};
+    return function(selector, rows, columns) {
+
+        var $selector = $(selector);
+        var $$table = $('<table>').appendTo($selector);
+
+
+    };
 }
 
 // SUBMIT THE CODE ABOVE THIS LINE
 
-if(typeof module !== 'undefined') {
-	module.exports = solve;
+if (typeof module !== 'undefined') {
+    module.exports = solve;
 }
